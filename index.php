a8d2017e1ebbbd4453162e617cf6291644bd88ab
Sirarpy <47724468+Sirarpy@users.noreply.github.com>
2019-02-19 16:23:59 +0400

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
-    <title>Home</title>
+    <title>Home page</title>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
